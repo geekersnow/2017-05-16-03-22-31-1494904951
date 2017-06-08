6c44da0e56907fd2d5625be7d2111399c3f7dfc3
““geekersnow” <kaishiaini1314@126.com>
2017-06-08 16:55:34 +0800

    first commit

--- main/main.js
+++ main/main.js
@@ -5,29 +5,22 @@
 module.exports = function main(num) {
 	//establish code list for number 0~9
     var number = ["._.", "...", "|.|", "..|", "._|", "|_|", "|_."];
-	var numcode = {
-		 '0':[number[0],number[2],number[5]],
-		 '1':[number[1],number[3],number[3]],
-		 '2':[number[0],number[4],number[6]],
-		 '3':[number[0],number[4],number[4]],
-		 '4':[number[1],number[5],number[3]],
-		 '5':[number[0],number[6],number[4]],
-		 '6':[number[0],number[6],number[5]],
-		 '7':[number[0],number[3],number[3]],
-		 '8':[number[0],number[5],number[5]],
-		 '9':[number[0],number[5],number[3]]
-	};
+	var numcode = [
+		 [number[0],number[1],number[0],number[0],number[1],number[0],number[0],number[0],number[0],number[0]],
+		 [number[2],number[3],number[4],number[4],number[5],number[6],number[6],number[3],number[5],number[5]],
+		 [number[5],number[3],number[6],number[4],number[3],number[4],number[5],number[3],number[5],number[3]]
+	];
 	//split num into single number 
 	var numarray = num.split('');
 	var actual = [];
 	
 	for (var i= 0; i < numarray.length; i++)
 	{
-		for(var j = 0; j < numcode[numarray[i]].length; j++)
+		for(var j = 0; j < 3; j++)
 		{	
-			actual.push(numcode[numarray[i]][j]);	
+			actual.push(numcode[numarray[i]][j]);
+			actual.join(' ');
 		}
-		actual.join(' ');
 		actual.push("\n");		
 	}			
     return actual;
