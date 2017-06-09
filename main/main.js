7e427504bc194dc1264e3c8f29c9c4e59298aec0
““geekersnow” <kaishiaini1314@126.com>
2017-06-09 10:59:52 +0800

    first commit

--- main/main.js
+++ main/main.js
@@ -3,26 +3,35 @@
 // |_|   ..|   |_.   ._|   ..|   ._|   |_|   ..|   |_|   ..|
 
 module.exports = function main(num) {
-	//establish code list for number 0~9
-    var number = ["._.", "...", "|.|", "..|", "._|", "|_|", "|_."];
-	var numcode = [
-		 [number[0],number[1],number[0],number[0],number[1],number[0],number[0],number[0],number[0],number[0]],
-		 [number[2],number[3],number[4],number[4],number[5],number[6],number[6],number[3],number[5],number[5]],
-		 [number[5],number[3],number[6],number[4],number[3],number[4],number[5],number[3],number[5],number[3]]
-	];
-	//split num into single number 
-	var numarray = num.split('');
-	var actual = [];
 	
-	for (var i= 0; i < numarray.length; i++)
+    var number = ["._.", "...", "|.|", "..|", "._|", "|_|", "|_."];
+	var numcode = {
+		 '0':[number[0],number[2],number[5]],
+		 '1':[number[1],number[3],number[3]],
+		 '2':[number[0],number[4],number[6]],
+		 '3':[number[0],number[4],number[4]],
+		 '4':[number[1],number[5],number[3]],
+		 '5':[number[0],number[6],number[4]],
+		 '6':[number[0],number[6],number[5]],
+		 '7':[number[0],number[3],number[3]],
+		 '8':[number[0],number[5],number[5]],
+		 '9':[number[0],number[5],number[3]]
+	};
+	
+	var numarray = num.split('');
+	var strarray = [];
+	var actual = " ";
+	
+	for (var i= 0; i < 3; i++)
 	{
-		for(var j = 0; j < 10; j++)
+		for(var j = 0; j < numarray.length; j++)
 		{	
-			var  temp = parseInt(numarray[j]); 
-			actual.push(numcode[temp][i]);
-			actual.join(' ');
+			console.log(numcode[numarray[j]][i]+"------");
+			strarray.push(numcode[numarray[j]][i]);
+			console.log(strarray+"---"+i+j);
 		}
-		actual.push("\n");		
-	}			
-    return actual;
+		actual = strarray.join(' ')+"\n";	
+	}
+	return actual;
 };
+		
